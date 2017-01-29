c246da8d4778c1dd6125b4acf8c43cb3eb4c2d7d
suzukiplan <suzukiplan.ys@gmail.com>
2017-01-29 21:46:06 +0900

    判定を入れる

--- VGSKeyboard/game.c
+++ VGSKeyboard/game.c
@@ -10,6 +10,8 @@
 #define MAX_NOTE 256
 #define NOTES_SPEED 256
 
+#define HITCHK(X1, Y1, XS1, YS1, X2, Y2, XS2, YS2) (X1 < X2 + XS2 && X2 < X1 + XS1 && Y1 < Y2 + YS2 && Y2 < Y1 + YS1)
+
 int KEY_FLAG[256];
 
 static int POS_MAP[24] = {
@@ -66,6 +68,34 @@ static int KEY_MAP[24] = {
     70  // A#2
 };
 
+static int KEY_MAP2[24] = {
+    48, // C 1
+    49, // C# 1
+    50, // D 1
+    51, // D# 1
+    52, // E 1
+    53, // F 1
+    54, // F# 1
+    55, // G 1
+    56, // G# 1
+    57, // A 1
+    58, // A# 1
+    59, // B 1
+    60, // C 2
+    61, // C# 2
+    62, // D 2
+    63, // D# 2
+    64, // E 2
+    65, // F 2
+    66, // F# 2
+    67, // G 2
+    68, // G# 2
+    69, // A 2
+    70, // A#2
+    71  // B 2
+
+};
+
 struct Note {
     int flag;
     int pos;
@@ -73,6 +103,13 @@ struct Note {
     int y;
     int x;
     int gx;
+};
+
+struct Bomb {
+    int flag;
+    int an;
+    int x;
+    int y;
 };
 
 struct ScoreElement {
@@ -94,9 +131,48 @@ struct ScoreElement KAERU[] = {
 struct GameTable {
     struct Note n[MAX_NOTE];
     int n_idx;
+    struct Bomb b[MAX_NOTE];
+    int b_idx;
+    int push[24];
+    int prev[24];
     int spos;
     int z;
 } TBL;
+
+void push_check() {
+    int i, now;
+    for (i = 0; i < 24; i++) {
+        now = KEY_FLAG[KEY_MAP2[i]];
+        if (now && !TBL.prev[i]) {
+            TBL.push[i] = 1;
+        } else {
+            TBL.push[i] = 0;
+        }
+        TBL.prev[i] = now;
+    }
+}
+
+void add_bomb(int pos, int y) {
+    TBL.b[TBL.b_idx].flag = 1;
+    TBL.b[TBL.b_idx].an = 0;
+    TBL.b[TBL.b_idx].x = POS_MAP[pos] + 20 + 1 - 4;
+    TBL.b[TBL.b_idx].y = y - 10;
+    TBL.b_idx++;
+    TBL.b_idx &= MAX_NOTE - 1;
+}
+
+void draw_bomb() {
+    int i;
+    for (i = 0; i < MAX_NOTE; i++) {
+        if (TBL.b[i].flag) {
+            vgs2_putSP(2, TBL.b[i].an / 2 * 24, 0, 24, 24, TBL.b[i].x, TBL.b[i].y);
+            TBL.b[i].an++;
+            if (20 <= TBL.b[i].an) {
+                TBL.b[i].flag = 0;
+            }
+        }
+    }
+}
 
 void add_note(int pos) {
     TBL.n[TBL.n_idx].flag = 1;
@@ -129,10 +205,36 @@ void draw_notes() {
     for (i = 0; i < MAX_NOTE; i++) {
         if (TBL.n[i].flag) {
             vgs2_putSP(1, TBL.n[i].gx, 0, 16, 4, TBL.n[i].x, TBL.n[i].y);
-            TBL.n[i].fy += NOTES_SPEED;
-            TBL.n[i].y = TBL.n[i].fy >> 8;
-            if (180 < TBL.n[i].y) {
-                TBL.n[i].flag = 0;
+            if (TBL.push[TBL.n[i].pos]) {
+                if (HITCHK(TBL.n[i].x, TBL.n[i].y, 16, 4, 0, 176, 320, 4)) {
+                    // perfect
+                    puts("perfect");
+                    TBL.n[i].flag = 0;
+                    add_bomb(TBL.n[i].pos, TBL.n[i].y);
+                } else if (HITCHK(TBL.n[i].x, TBL.n[i].y, 16, 4, 0, 174, 320, 8)) {
+                    // great
+                    puts("great");
+                    TBL.n[i].flag = 0;
+                    add_bomb(TBL.n[i].pos, TBL.n[i].y);
+                } else if (HITCHK(TBL.n[i].x, TBL.n[i].y, 16, 4, 0, 172, 320, 12)) {
+                    // good
+                    puts("good");
+                    TBL.n[i].flag = 0;
+                    add_bomb(TBL.n[i].pos, TBL.n[i].y);
+                } else if (HITCHK(TBL.n[i].x, TBL.n[i].y, 16, 4, 0, 170, 320, 16)) {
+                    // bad
+                    puts("bas");
+                    TBL.n[i].flag = 0;
+                    add_bomb(TBL.n[i].pos, TBL.n[i].y);
+                }
+            }
+            if (TBL.n[i].flag) {
+                TBL.n[i].fy += NOTES_SPEED;
+                TBL.n[i].y = TBL.n[i].fy >> 8;
+                if (200 < TBL.n[i].y) {
+                    TBL.n[i].flag = 0;
+                    // miss
+                }
             }
         }
     }
@@ -212,8 +314,11 @@ int vgs2_loop() {
             }
         }
     }
+    push_check();
     draw_lane(20, 180);
+    vgs2_boxSP(0, 176, 320, 179, 105);
     draw_notes();
+    draw_bomb();
     draw_keyboard(20, 180);
     return 0;
 }
