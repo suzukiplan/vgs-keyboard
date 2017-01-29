738d2f87cbed2b99c986a1c17e04a792aa130bac
suzukiplan <suzukiplan.ys@gmail.com>
2017-01-29 18:12:34 +0900

    とりあえず、ランダムで単発ノートを落としてみる(実験的)

--- VGSKeyboard/game.c
+++ VGSKeyboard/game.c
@@ -7,7 +7,37 @@
 //
 #include "vgs2.h"
 
+#define MAX_NOTE 256
+#define NOTES_SPEED 256
+
 int KEY_FLAG[256];
+
+static int POS_MAP[24] = {
+    0,   // C 1
+    10,  // C# 1
+    20,  // D 1
+    30,  // D# 1
+    40,  // E 1
+    60,  // F 1
+    70,  // F# 1
+    80,  // G 1
+    90,  // G# 1
+    100, // A 1
+    110, // A# 1
+    120, // B 1
+    140, // C 2
+    150, // C# 2
+    160, // D 2
+    170, // D# 2
+    180, // E 2
+    200, // F 2
+    210, // F# 2
+    220, // G 2
+    230, // G# 2
+    240, // A 2
+    250, // A# 2
+    260  // B 2
+};
 
 static int KEY_MAP[24] = {
     48, // C 1
@@ -35,6 +65,43 @@ static int KEY_MAP[24] = {
     68, // G# 2
     70  // A#2
 };
+
+struct Note {
+    int flag;
+    int pos;
+    int fy;
+    int y;
+    int x;
+};
+
+struct GameTable {
+    struct Note n[MAX_NOTE];
+    int n_idx;
+} TBL;
+
+void add_note(int pos) {
+    TBL.n[TBL.n_idx].flag = 1;
+    TBL.n[TBL.n_idx].pos = pos;
+    TBL.n[TBL.n_idx].x = POS_MAP[pos] + 20;
+    TBL.n[TBL.n_idx].y = -4;
+    TBL.n[TBL.n_idx].fy = TBL.n[TBL.n_idx].y * 256;
+    TBL.n_idx++;
+    TBL.n_idx &= MAX_NOTE - 1;
+}
+
+void draw_notes() {
+    int i;
+    for (i = 0; i < MAX_NOTE; i++) {
+        if (TBL.n[i].flag) {
+            vgs2_boxfSP(TBL.n[i].x, TBL.n[i].y, TBL.n[i].x + 16, TBL.n[i].y + 4, 111);
+            TBL.n[i].fy += NOTES_SPEED;
+            TBL.n[i].y = TBL.n[i].fy >> 8;
+            if (180 < TBL.n[i].y) {
+                TBL.n[i].flag = 0;
+            }
+        }
+    }
+}
 
 void draw_lane(int x, int y) {
     int i;
@@ -88,14 +155,17 @@ void draw_keyboard(int x, int y) {
     }
 }
 
-
 int vgs2_init() {
     vgs2_pallet256(0);
     return 0;
 }
 
 int vgs2_loop() {
+    static unsigned int frame;
+    frame++;
+    if (frame % 48 == 0) add_note(vgs2_rand() % 24);
     draw_lane(20, 180);
+    draw_notes();
     draw_keyboard(20, 180);
     return 0;
 }
